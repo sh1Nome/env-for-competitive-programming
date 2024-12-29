77782ff27eb94518c4e027afaee7aa5785cdc750
sh1Nome <shinodayo1024@gmail.com>
2024-12-29 18:09:56 +0900

    標準出力の自動フラッシュを無効化
    速度向上のため

--- work/Main.java
+++ work/Main.java
@@ -1,14 +1,16 @@
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            new Main().run(scanner);
+        try (Scanner scanner = new Scanner(System.in);
+                PrintWriter out = new PrintWriter(System.out, false);) {
+            new Main().run(scanner, out);
         }
     }
 
-    private void run(Scanner scanner) {
-        System.out.println(scanner.nextLine());
+    private void run(Scanner scanner, PrintWriter out) {
+        out.println(scanner.nextLine());
     }
 
 }
