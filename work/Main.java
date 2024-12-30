d494a291acbdf6d1811f468bcf66b85f255a7e99
sh1Nome <shinodayo1024@gmail.com>
2024-12-30 19:29:10 +0900

    場合の数ようの計算クラス

--- work/Main.java
+++ work/Main.java
@@ -10,7 +10,58 @@ public class Main {
     }
 
     private void run(Scanner scanner, PrintWriter out) {
-        out.println(scanner.nextLine());
+    }
+}
+
+/**
+ * 場合の数に関する計算クラス
+ */
+class Combinatorial {
+
+    /**
+     * n個の要素からr個を選び出す、組み合わせの総数を取得する
+     * 
+     * @param n
+     * @param r
+     * @return 組み合わせの総数
+     */
+     static long conbination(long n, long r) {
+        return permutation(n, r) / factorial(r);
+    }
+
+    /**
+     * n個の要素からr個を並べる、順列の総数を取得する
+     * 
+     * @param n
+     * @param r
+     * @return 順列の総数
+     */
+     static long permutation(long n, long r) {
+        return productNtoM(n, n - r + 1);
+    }
+
+    /**
+     * nの階乗を取得する
+     * 
+     * @param n
+     * @return nの階乗
+     */
+     static long factorial(long n) {
+        return permutation(n, n);
+    }
+
+    /**
+     * nからmまでのすべての数の積を取得する
+     * 
+     * @param n
+     * @param m
+     * @return nからmまでのすべての数の積
+     */
+     static long productNtoM(long n, long m) {
+        if (n == m) {
+            return m;
+        }
+        return n * productNtoM(n - 1, m);
     }
 
 }
